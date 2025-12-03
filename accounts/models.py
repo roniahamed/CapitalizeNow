ba658b6635c6eefc787baeaff6afa326e342f204
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-03 06:43:04 +0600

    add user model, serializers, and update settings for custom user authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class RoleChoices(models.TextChoices):
+    ADMIN = 'ADMIN', 'Admin'
+    GENERAL = 'GENERAL', 'General User'
+
+
+
+
+class User(AbstractUser):
+    role = models.CharField(max_length=50, choices=RoleChoices.choices, blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User
+from .serializers import UserSerializer, UserCreateSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
