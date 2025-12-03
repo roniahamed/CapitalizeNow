46e95597088d9076e7f70724b6c7d1a4ee21d8e1
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-03 06:45:24 +0600

    add initial migration for custom User model and implement user registration API

--- accounts/views.py
+++ accounts/views.py
@@ -4,5 +4,15 @@ from .serializers import UserSerializer, UserCreateSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 
+class UserRegister(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer =  UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
