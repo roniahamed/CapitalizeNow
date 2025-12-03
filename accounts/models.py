3de707b121d432273fb0d75e9fd19a39818e3ada
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-03 07:07:44 +0600

    fix RoleChoices constants to use lowercase values for consistency

--- accounts/models.py
+++ accounts/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class RoleChoices(models.TextChoices):
-    ADMIN = 'ADMIN', 'Admin'
-    GENERAL = 'GENERAL', 'General User'
+    ADMIN = 'admin', 'Admin'
+    GENERAL = 'general', 'General User'
 
 
 
 
 class User(AbstractUser):
+    email = None 
     role = models.CharField(max_length=50, choices=RoleChoices.choices, blank=True, null=True)
     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
 
