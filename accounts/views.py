3e7edc9e23639bce5cdeeb6f905a618825fc53e8
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-03 07:07:57 +0600

    refactor UserRegister to set default role and simplify validation logic

--- accounts/views.py
+++ accounts/views.py
@@ -5,14 +5,17 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from .models import RoleChoices
 
 
 class UserRegister(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, format=None):
+        role = request.data.pop('role', None)
+        role = RoleChoices.GENERAL
+        request.data['role'] = role
         serializer =  UserCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
